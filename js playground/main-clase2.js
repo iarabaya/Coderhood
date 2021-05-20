e122221ac1b04cde5b3e836305ceef84cae2c7ce
Iara Baya <iarabayav@gmail.com>
2021-05-20 18:23:11 -0300

    terminados ejercicios de contadores y fibonacci

--- js playground/main-clase2.js
+++ js playground/main-clase2.js
@@ -27,10 +27,50 @@ console.log(`los numeros impares ${impares}`);
 console.log(`los numeros impares menores a ${limite2/2} es ${imparesMenores}`);
 
 //-----Contador2
-var limite3 = 20;
-for (let index = 0; index < limite3; index++) {
-    
-    
+var limite3 = 30;
+var pares3 = 0;
+var final3 = 2;
+var multiplosDe3 = 0;
+var suma = 0;
+
+for (let index = 0; (index <= limite3) && (pares3 < final3); index++) {
+    if(index%2 === 0){
+        pares3++;
+        console.log(`${index} es numero par`);
+    }
+    if(index%3 === 0){
+        multiplosDe3 ++;
+        console.log(`${index} es multiplo de 3`);
+    }
+
+    suma = suma + index; 
 }
+ 
+console.log(`hay ${pares3} numeros pares`);
+console.log(`hay ${multiplosDe3} multiplos de 3`);
+console.log(`la suma de todos los numeros es de ${suma}`);
 
 //-----La secuencia de Fibonacci
+console.clear();
+
+const limite4 = 6;
+var num1 = 0;
+var num2 = 0;
+var suma4 = 0;
+
+for (let index = 0; index < limite4; index++) {
+    
+
+    suma4 = num1 + num2;
+    
+    console.log(num2);
+
+    if(num2 === 0){
+        num2 = 1;
+        
+    }else{
+        num1 = num2;
+        num2 = suma4;
+    }
+
+}
